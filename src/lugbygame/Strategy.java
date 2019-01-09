b4b9cda9226ae3c841be52699f6301e18928d8d2
sunghoon <sunghoon@sunghoon-H81-M1>
2019-01-09 18:57:48 +0000

    플레이어가 움직이지만 전략을 다시 짤 필요가 있음.
    충돌 감지는 원활히 되며, 아직 넉백 규칙은 적용하지 않음
    
    패스기능은 미구현
    
    status와 order의 데이터 전달 폼은 포팅 완료(좌우 반전 적용됨). -> y축 이동 점검 한 번 해보기(전략 B가
    지나치게 아래쪽으로 감)
    //status는 unit객체의 정보(x,y)만 제공됨.
    
    
    플레이어 이동 시 스테미너 감소 구현됨

--- src/lugbygame/Strategy.java
+++ src/lugbygame/Strategy.java
@@ -1,7 +1,12 @@
 package lugbygame;
 
 public interface Strategy {
-	//현재 상태(currentStatus)에 따라서 다른 전략(Order)을 생성
-	//전략(Order)은 각 플레이어의 다음 이동 벡터와 패스 대상에 대한 정보를 포함
-	public abstract Order execute(Status currentStatus);
+	// 현재 상태(currentStatus)에 따라서 다른 전략(Order)을 생성
+	// 전략(Order)은 각 플레이어의 다음 이동 벡터와 패스 대상에 대한 정보를 포함
+	public abstract void execute(Status currentStatus, Order resultOrder);
+
+	public default int getRandom(int m, int n) {
+		int d = Math.abs(m - n) + 1;
+		return (int) (Math.random() * d) + 1;
+	}
 }
--- src/lugbygame/TeamA.java
+++ src/lugbygame/TeamA.java
@@ -1,10 +1,90 @@
 package lugbygame;
 
 public class TeamA implements Strategy {
-	
+
 	@Override
-	public Order execute(Status currentStatus) {
-		// TODO Auto-generated method stub
-		return null;
+	public void execute(Status currentStatus, Order resultOrder) {
+		resultOrder.clear();
+
+		int i = 0, j, k;
+
+		resultOrder.passto = -1;
+
+		for (i = 0; i < 6; i++) {
+
+			resultOrder.dx[i] = getRandom(0, 2) - 1;
+			resultOrder.dy[i] = getRandom(0, 2) - 1;
+
+			if (currentStatus.ballOwner == i) {
+
+				for (j = 0; j < 6; j++) {
+					if (currentStatus.mine[i].x - currentStatus.enemy[j].x < 0
+							&& currentStatus.mine[i].x - currentStatus.enemy[j].x >= -2
+							&& currentStatus.mine[i].y - currentStatus.enemy[j].y <= 2
+							&& currentStatus.mine[i].y - currentStatus.enemy[j].y >= -2) {
+						// Pass
+						k = 0;
+						if (i == 5) {
+							resultOrder.passto = getRandom(0, 4);
+						} else if (currentStatus.mine[5].x <= currentStatus.mine[i].x) {
+							resultOrder.passto = 5;
+						} else
+							for (k = 0; k < 5; k++) {
+								if (currentStatus.mine[k].x <= currentStatus.mine[i].x) {
+									resultOrder.passto = k;
+									break;
+								}
+							}
+
+						if (k == 5) {
+							resultOrder.dx[i] = -1;
+						}
+						break;
+					}
+				}
+
+				if (j == 6) {
+					resultOrder.dx[i] = 1;
+					if (currentStatus.mine[i].y < (_Game.HEIGHT - 1) / 2 - 5)
+						resultOrder.dy[i] = 1;
+					else if (currentStatus.mine[i].y > (_Game.HEIGHT - 1) / 2 + 5)
+						resultOrder.dy[i] = -1;
+				}
+
+				for (j = 0; j < 6; j++) {
+					if (j != i && i != 5) {
+						if (currentStatus.mine[i].x < currentStatus.mine[j].x) {
+							resultOrder.dx[j] = -1;
+						} else if (currentStatus.mine[i].x > currentStatus.mine[j].x) {
+							resultOrder.dx[j] = 1;
+						}
+					}
+
+				}
+
+			}
+		}
+
+		if (currentStatus.ballOwner == -1 || currentStatus.ballOwner >= 6) {
+
+			for (i = 0; i < 6; i++) {
+				if (currentStatus.ball.x > currentStatus.mine[i].x)
+					resultOrder.dx[i] = 1;
+				else if (currentStatus.ball.x < currentStatus.mine[i].x)
+					resultOrder.dx[i] = -1;
+				else
+					resultOrder.dx[i] = 0;
+
+				if (currentStatus.ball.y > currentStatus.mine[i].y)
+					resultOrder.dy[i] = 1;
+				else if (currentStatus.ball.y < currentStatus.mine[i].y)
+					resultOrder.dy[i] = -1;
+				else
+					resultOrder.dy[i] = 0;
+			}
+
+		}
+
+		return;
 	}
 }
